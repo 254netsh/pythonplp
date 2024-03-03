4db70630f9dd360ff4062477dca10f10699cf498
sam <samgachuiga@gmail.com>
2024-03-03 10:25:08 +0300

    Signed-off-by: sam <samgachuiga@gmail.com>

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name= input("What is your name?")
+age = input("How old are you?")
+location = input("where is your current residense")
+
+print("How are you", name ,"we confirm you are ", age, "years old And you currently live in ", location )
